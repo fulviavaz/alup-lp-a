4e56a68ee940ab9ee2f25b73a0bb85eca584edcf
fulviavaz <fulviavaz.dev@gmail.com>
2024-12-06 22:14:33 -0300

    style: slider responsiveness

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,25 +1,28 @@
 var swiper = new Swiper(".swiper", {
     loop: true,
-    grabCursor: true,
       pagination: {
          el: ".swiper-pagination",
           clickable: true,
      },
-//        breakpoints: {
-//     // when window width is >= 320px
-//     320: {
-//       slidesPerView: 2,
-//       spaceBetween: 20
-//     },
-//     // when window width is >= 480px
-//     480: {
-//       slidesPerView: 3,
-//       spaceBetween: 30
-//     },
-//     // when window width is >= 640px
-//     640: {
-//       slidesPerView: 4,
-//       spaceBetween: 40
-//     }
-//   },     
+       breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 40
+           },
+           1280: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    }
+  },     
     });
