b476455c282c591cf223fb55cdb3110247054565
fulviavaz <fulviavaz.dev@gmail.com>
2024-12-07 09:15:46 -0300

    feat: logos slider

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -25,4 +25,33 @@ var swiper = new Swiper(".swiper", {
       spaceBetween: 40
     }
   },     
+});
+    
+var swiper = new Swiper(".mySwiperLogos", {
+    loop: true,
+      pagination: {
+         el: ".swiper-pagination",
+          clickable: true,
+     },
+       breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 4,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 30
+           },
+           1280: {
+      slidesPerView: 6,
+      spaceBetween: 40
+    }
+  },     
     });
